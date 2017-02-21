5666c2e5fef1d406886a5dcdcf2df733f4e21ca9
anakinlau <wcl0004@auburn.edu>
2017-02-21 15:06:18 -0600

    Red Light |

--- softwareprocess/test/SampleTest.py
+++ softwareprocess/test/SampleTest.py
@@ -250,7 +250,7 @@ class SampleTest(unittest.TestCase):
 
     def test700_010_ShouldBeOneHalve(self):
         mySample = SM.Sample(self.nominalN)
-        def f(u, n):
+        def f1(u, n):
             return u**2
         self.assertAlmostEquals(mySample.integrate(0, 1.0, 2, f1), 0.33333333)
         #self.assertAlmostEquals(mySample.p(1.0, 1), 0.33333333)
