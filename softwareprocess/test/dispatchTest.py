2c5e9ad784d7cd1aebeb4b02c602fec7bac8227b
anakinlau <wcl0004@auburn.edu>
2017-03-23 20:57:43 -0500

    Red Light |

--- softwareprocess/test/dispatchTest.py
+++ softwareprocess/test/dispatchTest.py
@@ -55,10 +55,12 @@ class dispatchTest(unittest.TestCase):
         self.assertEquals(expectedString, DSP.dispatch({'observation': '42d0.0',  'op': 'adjust'}))
 
     def test201_011_ShouldReturnAltitude(self):
-        expectedString = {'altitude':'41d59.0', 'observation': '42d0.0',  'op': 'adjust'}
-        self.assertEquals(expectedString, DSP.dispatch({'observation': '45d15.2', 'height': '6',
-                                                        'pressure': '1010', 'horizon': 'natural',
-                                                        'op': 'adjust', 'temperature': '71'}))
+        expectedString = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0',
+                          'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust',
+                          'temperature': '85'}
+        self.assertEquals(expectedString, DSP.dispatch({'observation': '30d1.5', 'height': '19.0',
+                                                        'pressure': '1000', 'horizon': 'artificial',
+                                                        'op': 'adjust', 'temperature': '85'}))
 
     # Should change the string to have added default params if not already there
     def test202_100_ShouldReturnWithDefaultParam(self):
