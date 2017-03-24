8a9fee8b6a447c4b324e732c71fdf708322a699c
anakinlau <wcl0004@auburn.edu>
2017-03-23 22:16:09 -0500

    Green Light |

--- softwareprocess/test/dispatchTest.py
+++ softwareprocess/test/dispatchTest.py
@@ -103,7 +103,7 @@ class dispatchTest(unittest.TestCase):
                                                         'temperature': 'a'}))
 
     def test201_017_ShouldReturnAltitude(self):
-        expectedString = {'observation': '15d04.9', 'height': '6.0', 'pressure': '1010',
+        expectedString = {'altitude': '14d59.1', 'observation': '15d04.9', 'height': '6.0', 'pressure': '1010',
                           'horizon': 'natural', 'op': 'adjust', 'temperature': '72'}
         self.assertEquals(expectedString, DSP.dispatch({'op':'adjust', 'observation':'15d04.9', 'height':'6.0',
                                                         'temperature':'72','pressure':'1010'}))
