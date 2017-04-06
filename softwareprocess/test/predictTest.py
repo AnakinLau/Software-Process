6bf8e1e0e12f1d3249fde4fc13f644c4b55f36a2
ARCH_DCMP <ARCH_DCMP@AnakinsLaptop>
2017-04-06 01:51:39 -0500

    Red Light |

--- softwareprocess/test/predictTest.py
+++ softwareprocess/test/predictTest.py
@@ -187,4 +187,4 @@ class predictTest(unittest.TestCase):
         timeInput = '22:02:23'
         dateInput = '2002-02-02'
         expectedCumProg = '-3d34.8'
-        self.assertEquals(False, DSP.getAdjustedGHA(dateInput, timeInput));
+        self.assertEquals(expectedCumProg, DSP.getAdjustedGHA(dateInput, timeInput));
