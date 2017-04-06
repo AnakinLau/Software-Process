6f24d3b1ae98dfd0345ea07e3a5888f2dea27eb0
ARCH_DCMP <ARCH_DCMP@AnakinsLaptop>
2017-04-05 23:08:21 -0500

    Red Light |

--- softwareprocess/test/predictTest.py
+++ softwareprocess/test/predictTest.py
@@ -47,7 +47,8 @@ class predictTest(unittest.TestCase):
         expectedString = {'error': 'invalid date'}
         self.assertEquals(expectedString['error'],
                           DSP.dispatch({'op': 'predict', 'body': 'Betelgeuse',
-                                        'date': '2001-01-24'})['error'])
+                                        'date': '200A-01-24'})['error'])
+
 # Should change the string to have added default params if not already there
     def test202_100_ShouldReturnWithDefaultParam(self):
         entryDict = {'body': 'Betelgeuse',  'op': 'predict'}
