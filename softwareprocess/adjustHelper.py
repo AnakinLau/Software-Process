0a514e557644ebf8ef89646ba9a9def4ada54225
anakinlau <wcl0004@auburn.edu>
2017-04-07 03:21:44 -0500

    Refactor |

---
+++ softwareprocess/adjustHelper.py
@@ -0,0 +1,111 @@
+import math
+import softwareprocess.conversionHelper as CH
+
+def adjustHelper():
+
+    def checkObservationFormat(observation):
+        if(observation.count('d') != 1):
+            return False
+        posOfd = observation.find('d')
+        degString = observation[0: posOfd]
+        minString = observation[posOfd + 1: len(observation)]
+
+        if(minString.count('.') != 1):
+            return False
+        posOfPeriod = minString.find('.')
+        yInt = minString[0: posOfPeriod]
+        yDecimal = minString[posOfPeriod + 1 : len(minString)]
+        if(len(yDecimal) != 1):
+            return False
+        if(len(yInt) > 2 or len(yInt) == 0):
+            return False
+        if(not(yDecimal.isdigit())):
+            return False
+        if(not(yInt.isdigit())):
+            return False
+        if(int(yInt) > 59 or int(yInt) < 0):
+            return  False
+
+        # check if deg is a digit first
+        if(len(degString) > 2 or len(degString) == 0):
+            return False
+        if(not(degString.isdigit())):
+            return False
+        if(int(degString) < 0 or int(degString) >= 90):
+            return False
+        return True
+
+
+    def checkHeightFormat(height):
+        try:
+            if(float(height) >= 0):
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+
+
+    def checkTemperatureFormat(temperature):
+        try:
+            if(float(temperature) >= -20 and float(temperature) <= 120
+               and temperature.count('.') == 0):
+
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+
+
+    def checkPressureFormat(pressure):
+        try:
+            if(float(pressure) >= 100 and float(pressure) <= 1100
+               and pressure.count('.') == 0):
+
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+
+
+    def checkHorizonFormat(horizon):
+        try:
+            if(horizon.lower() == 'natural' or horizon.lower() == 'artificial'):
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+
+
+    def getDip(dictInput):
+        if(dictInput['horizon'].lower() == 'natural'):
+            return float((-0.97 * math.sqrt(float(dictInput['height']))) / 60)
+        else :
+            return 0
+
+
+    def getRefraction(dictInput):
+        refractionP1 = (-0.00452 * int(dictInput['pressure']))
+        refractionP2 = (273 + ((int(dictInput['temperature']) - 32) / 1.8))
+        degToInt = CH.getObservationDegToInt(dictInput['observation'])
+        minToFloat = CH.getObservationMinToFloat(dictInput['observation'])
+        print("minToFloat= {0}".format(minToFloat))
+        refractionP3 = (math.tan(math.radians(degToInt +
+                                              (CH.convertMinToNumber(minToFloat)))))
+        print("refractionP1= {0}, refractionP2= {1}, "
+              "refractionP3= {2}".format(refractionP1, refractionP2, refractionP3))
+        return refractionP1 / refractionP2 / refractionP3
+
+
+    def getAltitude(dictInput):
+        dip = getDip(dictInput)
+        refraction = getRefraction(dictInput)
+        degInt = CH.getObservationDegToInt(dictInput['observation'])
+        minFloat = CH.getObservationMinToFloat(dictInput['observation'])
+        degMinNum = CH.convertDegMinToNumber(degInt, minFloat)
+        degMinNum = degMinNum + dip + refraction
+        print("degMinNum= {0}".format(degMinNum))
+        return CH.convertNumToDegMinString(degMinNum)
---
+++ softwareprocess/conversionHelper.py
@@ -0,0 +1,67 @@
+import math
+
+def conversionHelper():
+
+    # Helper methods getting degrees and minutes
+    # Does not work with a negative zero~
+    def getObservationDegToInt(observationInput):
+        posOfd = observationInput.find('d')
+        degString = observationInput[0: posOfd]
+        print('degString= {0}'.format(degString))
+        if(degString == '-0'):
+            return -0.0
+        return int(degString)
+
+
+    def getObservationMinToFloat(observationInput):
+        posOfd = observationInput.find('d')
+        minString = observationInput[posOfd + 1: len(observationInput)]
+        return float(minString)
+
+
+    def convertMinToNumber(minNum):
+        return float(minNum/60)
+
+    # Does not work with a negative zero~
+    def convertDegMinToNumber(degInput, minInput):
+        print('convertDegMinToNumber degInput= {0}'.format(degInput))
+        if(degInput < 0 ):
+            return degInput - convertMinToNumber(minInput)
+        else:
+            return degInput + convertMinToNumber(minInput)
+
+    def convertDegMinStrToNumber(degMinStr):
+        posOfd = degMinStr.find('d')
+        degString = degMinStr[0: posOfd]
+        if(degString == '-0'):
+            return convertDegMinToNumber(-1,
+                              getObservationMinToFloat(degMinStr)) +1
+        else:
+            return convertDegMinToNumber(getObservationDegToInt(degMinStr),
+                              getObservationMinToFloat(degMinStr))
+
+    def convertNumToDegMinString(numInput):
+        print ("convertNumToDegMinString numInput= {0}".format(numInput))
+        if numInput >= 0:
+            decimalNum = float(numInput % 1)
+        else:
+            decimalNum = float((numInput * -1) % 1)
+        deg = 0
+        mins = round((decimalNum * 60),1)
+        leftOver = mins / 60
+        if(leftOver >= 1):
+            print ("leftOver {0} is >= 1")
+            deg = int(leftOver)
+            mins = mins - (60 * leftOver)
+        if(numInput < 0):
+            deg = -1 * deg
+        deg = deg + int(numInput)
+
+        # make sure if it's bigger than 360deg it is chopped
+        if(deg > 360):
+            deg = deg % 360
+        elif (deg < -360 ):
+            deg = deg * -1
+            deg = (deg % 360) * -1
+
+        return str(deg) + 'd' + str(mins)
