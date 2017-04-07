808dafe30525f7a2f43737fb4564c81fedef4bb8
anakinlau <wcl0004@auburn.edu>
2017-04-06 23:16:56 -0500

    Refactor |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -233,7 +233,6 @@ def convertMinToNumber(minNum):
 def convertDegMinToNumber(degInput, minInput):
     print('degInput= {0}'.format(degInput))
     if(degInput == -0.0 or degInput < 0 ):
-        degInput = 0
         return degInput - convertMinToNumber(minInput)
     else:
         return degInput + convertMinToNumber(minInput)
